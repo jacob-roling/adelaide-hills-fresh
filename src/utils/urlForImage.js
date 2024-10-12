138fc64f3d0fa1e2c2be21fe1d2c695d2e3142c8
Jacob <jacob.roling@hey.com>
2024-10-12 14:04:05 +1030

    chore: savepoint

--- src/utils/urlForImage.js
+++ src/utils/urlForImage.js
@@ -4,8 +4,8 @@ import imageUrlBuilder from "@sanity/image-url";
 const builder = imageUrlBuilder(sanityClient);
 
 // /**
-//  * 
-//  * @param {import("@sanity/image-url").SanityImageSource} source 
+//  *
+//  * @param {import("@sanity/image-url").SanityImageSource} source
 //  * @returns {import("@sanity/image-url").types.ImageUrlBuilder}
 //  */
 export default function urlForImage(source) {
